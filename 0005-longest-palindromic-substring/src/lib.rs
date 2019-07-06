20f1168cac1713b7bfb2ae8d96656a00bdd6b438
Toru Kobayashi <koba0004@gmail.com>
2019-07-06 17:12:02 +0900

    implemnt 5. Longest Palindromic Substring to pass examples

--- 0005-longest-palindromic-substring/src/lib.rs
+++ 0005-longest-palindromic-substring/src/lib.rs
@@ -1,5 +1,33 @@
+fn palindromic_substring(s: &String) -> bool {
+    let mut r = String::from("");
+    let iter = s.chars().rev();
+    for c in iter {
+        r.push(c);
+    }
+    *s == r
+}
+
 pub fn longest_palindrome(s: String) -> String {
-    s
+    let mut answer = String::from("");
+    let mut current = String::from("");
+    let chars: Vec<char> = s.chars().collect();
+    let last_index = chars.len() - 1;
+    let mut start_index = 0;
+    let mut i = 0;
+    while start_index < last_index {
+        current.push(chars[i]);
+        if answer.len() < current.len() && palindromic_substring(&current) {
+            // TODO: should not clone
+            answer = current.clone();
+        }
+        i = i + 1;
+        if i > last_index {
+            start_index = start_index + 1;
+            i = start_index;
+            current = String::from("");
+        }
+    }
+    answer
 }
 
 
