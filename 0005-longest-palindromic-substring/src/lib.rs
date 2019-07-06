db647bde845d4563e3aecf341ed8bf90a7ec51f1
Toru Kobayashi <koba0004@gmail.com>
2019-07-06 17:22:33 +0900

    implemnt 5. Longest Palindromic Substring to pass example3, 4

--- 0005-longest-palindromic-substring/src/lib.rs
+++ 0005-longest-palindromic-substring/src/lib.rs
@@ -11,17 +11,17 @@ pub fn longest_palindrome(s: String) -> String {
     let mut answer = String::from("");
     let mut current = String::from("");
     let chars: Vec<char> = s.chars().collect();
-    let last_index = chars.len() - 1;
+    let len = chars.len();
     let mut start_index = 0;
     let mut i = 0;
-    while start_index < last_index {
+    while start_index < len {
         current.push(chars[i]);
         if answer.len() < current.len() && palindromic_substring(&current) {
             // TODO: should not clone
             answer = current.clone();
         }
         i = i + 1;
-        if i > last_index {
+        if i >= len {
             start_index = start_index + 1;
             i = start_index;
             current = String::from("");
@@ -42,5 +42,22 @@ mod tests {
     #[test]
     fn example2() {
         assert_eq!(longest_palindrome(String::from("cbbd")), "bb");
+    }
+    #[test]
+    fn example3() {
+        assert_eq!(longest_palindrome(String::from("")), "");
+    }
+    #[test]
+    fn example4() {
+        assert_eq!(longest_palindrome(String::from("a")), "a");
+    }
+    #[test]
+    fn example5() {
+        assert_eq!(
+            longest_palindrome(
+                String::from("jrjnbctoqgzimtoklkxcknwmhiztomaofwwzjnhrijwkgmwwuazcowskjhitejnvtblqyepxispasrgvgzqlvrmvhxusiqqzzibcyhpnruhrgbzsmlsuacwptmzxuewnjzmwxbdzqyvsjzxiecsnkdibudtvthzlizralpaowsbakzconeuwwpsqynaxqmgngzpovauxsqgypinywwtmekzhhlzaeatbzryreuttgwfqmmpeywtvpssznkwhzuqewuqtfuflttjcxrhwexvtxjihunpywerkktbvlsyomkxuwrqqmbmzjbfytdddnkasmdyukawrzrnhdmaefzltddipcrhuchvdcoegamlfifzistnplqabtazunlelslicrkuuhosoyduhootlwsbtxautewkvnvlbtixkmxhngidxecehslqjpcdrtlqswmyghmwlttjecvbueswsixoxmymcepbmuwtzanmvujmalyghzkvtoxynyusbpzpolaplsgrunpfgdbbtvtkahqmmlbxzcfznvhxsiytlsxmmtqiudyjlnbkzvtbqdsknsrknsykqzucevgmmcoanilsyyklpbxqosoquolvytefhvozwtwcrmbnyijbammlzrgalrymyfpysbqpjwzirsfknnyseiujadovngogvptphuyzkrwgjqwdhtvgxnmxuheofplizpxijfytfabx")
+            ),
+            "qosoq"
+        );
     }
 }
