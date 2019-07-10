8c456598303e46b9a72c1d343ea59fd56042f704
Toru Kobayashi <koba0004@gmail.com>
2019-07-10 21:29:02 +0900

    pass the examples for 6. igZag Conversion!

--- 0006-zig-zag-conversion/src/lib.rs
+++ 0006-zig-zag-conversion/src/lib.rs
@@ -1,5 +1,47 @@
 pub fn convert(s: String, num_rows: i32) -> String {
-    s
+    let mut matrix: Vec<Vec<char>> = vec![];
+    let size = s.len();
+    for _ in 0..size {
+        let mut column: Vec<char> = vec![];
+        for _ in 0..num_rows {
+            column.push(' ');
+        }
+        matrix.push(column);
+    }
+    let chars: Vec<char> = s.chars().collect();
+    let mut column: i32 = 0;
+    let mut row: i32 = 0;
+    let mut direction: i32 = 1;
+    let max = num_rows - 1;
+    let mut initial = true;
+    // println!("matrix {:?}", matrix);
+    for c in chars {
+        // println!("column:{}, row:{}, direction:{}", column, row, direction);
+        matrix[column as usize][row as usize] = c;
+        if row == max && direction == 1 {
+            direction = -1;
+            column = column + 1;
+            row = row + direction;
+        } else if !initial && direction == - 1 && row == 1 {
+            column = column + 1;
+            row = 0;
+            direction = 1;
+        } else {
+            row = row + direction;
+        }
+        initial = false
+    }
+
+    let mut answer = String::new();
+    for i in 0..num_rows {
+        for m in &matrix {
+            if m[i as usize] != ' ' {
+                answer.push(m[i as usize]);
+            }
+        }
+    }
+    // println!("matrix {:?}", matrix);
+    answer
 }
 
 #[cfg(test)]
