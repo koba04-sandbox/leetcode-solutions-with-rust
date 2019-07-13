659b15efe13f49a8d5a2707bdeea9de96fcbaead
Toru Kobayashi <koba0004@gmail.com>
2019-07-14 00:42:31 +0900

    all tests are passed! 7.Reverse Integer

--- 0007-reverse-integer/src/lib.rs
+++ 0007-reverse-integer/src/lib.rs
@@ -1,5 +1,29 @@
 pub fn reverse(x: i32) -> i32 {
-    x
+    let sign = if x < 0 { -1 } else { 1 };
+    let mut nums: Vec<char> = x
+        .to_string()
+        .chars()
+        .collect()
+        ;
+    nums.reverse();
+    if sign == -1 {
+        nums.pop();
+    }
+
+    let mut is_first = true;
+    let mut answer = String::new();
+    for n in nums {
+        if is_first && n == '0' {
+            continue;
+        }
+        is_first = false;
+        answer.push(n);
+    }
+    // println!("answer is {}", answer);
+    match answer.parse::<i32>() {
+        Ok(n) => n * sign,
+        Err(_) => 0
+    }
 }
 
 #[cfg(test)]
@@ -19,5 +43,15 @@ mod tests {
     #[test]
     fn example3() {
         assert_eq!(reverse(120), 21);
+    }
+
+    #[test]
+    fn example4() {
+        assert_eq!(reverse(0), 0);
+    }
+
+    #[test]
+    fn example5() {
+        assert_eq!(reverse(1534236469), 0);
     }
 }
