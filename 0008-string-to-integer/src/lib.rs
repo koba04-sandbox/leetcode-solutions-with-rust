2e5741aa41092d18333e3155837c86be8d12cf88
Toru Kobayashi <koba0004@gmail.com>
2019-07-15 16:43:44 +0900

    passed basic examples for 0008

--- 0008-string-to-integer/src/lib.rs
+++ 0008-string-to-integer/src/lib.rs
@@ -1,5 +1,38 @@
 pub fn my_atoi(str: String) -> i32 {
-    str.parse::<i32>().unwrap()
+    let chars = str.chars();
+    let mut answer = String::new();
+    let mut is_start = true;
+    let mut is_passed_number = false;
+    for c in chars {
+        if c == ' ' {
+            continue;
+        }
+        if is_start {
+            if c == '-' || c == '+' {
+                is_start = false;
+                answer.push(c);
+                continue;
+            }
+            if !c.is_ascii_digit() {
+                answer = String::from("0");
+                break;
+            }
+        } else {
+            if !c.is_ascii_digit() {
+                break;
+            }
+        }
+        is_start = false;
+        is_passed_number = true;
+        answer.push(c);
+    }
+    if is_passed_number == false {
+        return 0;
+    }
+    match answer.parse::<i32>() {
+        Ok(a) => a,
+        Err(_) => std::i32::MIN
+    }
 }
 
 #[cfg(test)]
