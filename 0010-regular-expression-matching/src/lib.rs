6a8e66390b5402e78d86cf1230dc0fa2f9287f5c
Toru Kobayashi <koba0004@gmail.com>
2019-08-14 00:08:03 +0900

    refactor: variable names

--- 0010-regular-expression-matching/src/lib.rs
+++ 0010-regular-expression-matching/src/lib.rs
@@ -4,43 +4,48 @@ enum Mode {
     Asterisk,
 }
 
-pub fn is_match(s: String, p: String) -> bool {
-    let mut s: Vec<char> = s.chars().collect();
-    let mut p: Vec<char> = p.chars().collect();
+pub fn is_match(source: String, regexp: String) -> bool {
+    let mut sources: Vec<char> = source.chars().collect();
+    sources.reverse();
+    let mut regexps: Vec<char> = regexp.chars().collect();
+    regexps.reverse();
     let mut result = true;
     let mut current_mode;
-    loop {
-        if let Some(b) = s.pop() {
-            if let Some(a) = p.pop() {
-                if a == '.' {
-                    current_mode = Mode::Wildcard;
-                } else if a == '*' {
-                    current_mode = Mode::Asterisk;
-                } else {
-                    current_mode = Mode::Char(a);
-                }
-                match current_mode {
-                    Mode::Char(a) => {
-                        if a != b {
-                            println!("not match {}, {}", a, b);
-                            result = false;
-                            break;
-                        }
-                    },
-                    Mode::Asterisk => {
-                        // noop
-                    },
-                    Mode::Wildcard => {
-                        // noop
-                    }
-                }
-            } else {
-                result = false;
-                break;
-            }
+    let mut source_index = 0;
+    let mut regexp_index = 0;
+    while source_index < sources.len() && regexp_index < regexps.len() {
+        let source = sources[source_index];
+        let regexp = regexps[regexp_index];
+        println!("{}, {}", source, regexp);
+
+        source_index = source_index + 1;
+        regexp_index = regexp_index + 1;
+
+        if regexp == '.' {
+            current_mode = Mode::Wildcard;
+        } else if regexp == '*' {
+            current_mode = Mode::Asterisk;
         } else {
-            break;
+            current_mode = Mode::Char(regexp);
         }
+        match current_mode {
+            Mode::Char(regexp) => {
+                if source != regexp {
+                    println!("not match {}, {}", source, regexp);
+                    result = false;
+                    break;
+                }
+            },
+            Mode::Asterisk => {
+                // noop
+            },
+            Mode::Wildcard => {
+                // noop
+            }
+        }
+    }
+    if source_index != sources.len() || regexp_index != regexp.len() {
+        result = false;
     }
     result
 }
@@ -68,5 +73,9 @@ mod tests {
     #[test]
     fn example5() {
         assert_eq!(is_match(String::from("mississippi"), String::from("mis*is*p*.")), false);
+    }
+    #[test]
+    fn example6() {
+        assert_eq!(is_match(String::from("aaa"), String::from("aaaa")), false);
     }
 }
