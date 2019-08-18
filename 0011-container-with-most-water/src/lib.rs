e5562733753bfae0b3114d0ef41e09d15ebecce8
Toru Kobayashi <koba0004@gmail.com>
2019-08-18 14:51:26 +0900

    feat: implement a solution to pass the example1

--- 0011-container-with-most-water/src/lib.rs
+++ 0011-container-with-most-water/src/lib.rs
@@ -2,7 +2,22 @@ pub struct Solution {}
 
 impl Solution {
     pub fn max_area(height: Vec<i32>) -> i32 {
-        height[0]
+        let mut max = 0;
+        let mut i = 0;
+        while i < height.len() {
+            let mut inner_index = i + 1;
+            while inner_index < height.len() - 1 {
+                inner_index = inner_index + 1;
+                let x = inner_index - i;
+                let y = if height[i] < height[inner_index] { height[i] } else { height[inner_index] };
+                let area = x as i32 * y;
+                if area > max {
+                    max = area;
+                }
+            }
+            i = i + 1;
+        }
+        max
     }
 }
 
