7858c30af97c012ffe5a857dc5e430423ade3e76
Toru Kobayashi <koba0004@gmail.com>
2019-08-18 14:57:57 +0900

    feat: all tests for 0011-container-with-most-water are passed!

--- 0011-container-with-most-water/src/lib.rs
+++ 0011-container-with-most-water/src/lib.rs
@@ -6,14 +6,15 @@ impl Solution {
         let mut i = 0;
         while i < height.len() {
             let mut inner_index = i + 1;
-            while inner_index < height.len() - 1 {
-                inner_index = inner_index + 1;
+            while inner_index < height.len(){
                 let x = inner_index - i;
                 let y = if height[i] < height[inner_index] { height[i] } else { height[inner_index] };
+                // println!("{}, {}", x, y);
                 let area = x as i32 * y;
                 if area > max {
                     max = area;
                 }
+                inner_index = inner_index + 1;
             }
             i = i + 1;
         }
@@ -28,5 +29,9 @@ mod tests {
     #[test]
     fn example1() {
         assert_eq!(Solution::max_area(vec![1,8,6,2,5,4,8,3,7]), 49);
+    }
+    #[test]
+    fn example2() {
+        assert_eq!(Solution::max_area(vec![1,1]), 1);
     }
 }
