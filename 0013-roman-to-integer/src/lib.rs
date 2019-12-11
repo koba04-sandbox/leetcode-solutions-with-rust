3d5a49c2ab3a55c7de61266b2d71f48d855adfa3
Toru Kobayashi <koba0004@gmail.com>
2019-12-12 00:21:26 +0900

    13: all tests have been passed!

--- 0013-roman-to-integer/src/lib.rs
+++ 0013-roman-to-integer/src/lib.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 
 pub struct Solution {}
 
@@ -12,9 +13,54 @@ pub struct Solution {}
 // X can be placed before L (50) and C (100) to make 40 and 90.
 // C can be placed before D (500) and M (1000) to make 400 and 900.
 
+
 impl Solution {
     pub fn roman_to_int(s: String) -> i32 {
-        1
+        let mut roman_map = HashMap::new();
+        roman_map.insert('I', 1);
+        roman_map.insert('V', 5);
+        roman_map.insert('X', 10);
+        roman_map.insert('L', 50);
+        roman_map.insert('C', 100);
+        roman_map.insert('D', 500);
+        roman_map.insert('M', 1000);
+
+        let mut answer = 0;
+        let chars: Vec<char> = s.chars().rev().collect();
+        let mut i = 0;
+        while i < chars.len() {
+            let c = chars[i];
+            let n = roman_map.get(&c).unwrap();
+            if i != chars.len() - 1 {
+                let next = chars[i + 1];
+                match c {
+                    'X' | 'V' => {
+                        if next == 'I' {
+                            answer = answer - 1;
+                            i = i + 1;
+                        }
+                    },
+                    'C' | 'L' => {
+                        if next == 'X' {
+                            answer = answer - 10;
+                            i = i + 1;
+                        }
+                    },
+                    'M' | 'D' => {
+                        if next == 'C' {
+                            answer = answer - 100;
+                            i = i + 1;
+                        }
+
+                    },
+                    _ => {}
+                }
+            }
+
+            answer = answer + n;
+            i = i + 1;
+        }
+        answer
     }
 }
 
