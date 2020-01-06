d2028bc286ff56136ff743c3bba8782a6a08b91f
Toru Kobayashi <koba0004@gmail.com>
2020-01-07 01:05:56 +0900

    Implement 15. 3Sum, but Time Limit Exceeded

--- 0015-3sum/src/lib.rs
+++ 0015-3sum/src/lib.rs
@@ -2,7 +2,36 @@ pub struct Solution {}
 
 impl Solution {
     pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
-        vec![vec![0]]
+        let len = nums.len();
+        let mut answers = vec![];
+        for i in 0..len {
+            for j in i + 1..len {
+                for k in j + 1..len {
+                    // println!("{}:{}:{}", i, j, k);
+                    let ans = nums[i] + nums[j] + nums[k];
+                    if ans == 0 {
+                        let mut answer = vec![nums[i], nums[j], nums[k]];
+                        answer.sort();
+                        if Solution::check(&answers, &answer) == false {
+                            answers.push(answer);
+                        }
+                    }
+                }
+            }
+        }
+        answers
+    }
+    fn check(answers: &Vec<Vec<i32>>, candidate: &Vec<i32>) -> bool {
+        // println!("compare {:?}:{:?}", answers, candidate);
+        'outer: for answer in answers {
+            for (i, a) in answer.iter().enumerate() {
+                if a != &candidate[i] {
+                    continue 'outer;
+                }
+            }
+            return true;
+        }
+        false
     }
 }
 
