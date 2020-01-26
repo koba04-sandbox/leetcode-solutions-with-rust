e3e68a842fb6f3a5aa684871621fbde8a11e9289
Toru Kobayashi <koba0004@gmail.com>
2020-01-26 11:15:20 +0900

    wip

--- 0017-letter-combinations-of-a-phone-number/src/lib.rs
+++ 0017-letter-combinations-of-a-phone-number/src/lib.rs
@@ -1,8 +1,25 @@
+use std::collections::HashMap;
 pub struct Solution {}
+
 
 impl Solution {
     pub fn letter_combinations(digits: String) -> Vec<String> {
-        vec![]
+        let mut digitMap: HashMap<char, Vec<char>> = HashMap::new();
+        digitMap.insert('2', vec!['a', 'b', 'c']);
+        digitMap.insert('3', vec!['d', 'e', 'f']);
+        digitMap.insert('4', vec!['g', 'h', 'i']);
+        digitMap.insert('5', vec!['j', 'k', 'l']);
+        digitMap.insert('6', vec!['m', 'n', 'o']);
+        digitMap.insert('7', vec!['p', 'q', 'r', 's']);
+        digitMap.insert('8', vec!['t', 'u', 'v']);
+        digitMap.insert('9', vec!['w', 'x', 'y', 'z']);
+
+        let mut answers = vec![];
+
+        let chars: Vec<char> = digits.chars().collect();
+        let len = chars.len();
+
+        answers
     }
 }
 
