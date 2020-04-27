4d5fcdf5946d57b52d97ed126afd7d928538b166
Toru Kobayashi <koba0004@gmail.com>
2020-04-27 21:44:24 +0900

    feat(0020): all tests have been passed

--- 0020-valid-parenttheses/src/lib.rs
+++ 0020-valid-parenttheses/src/lib.rs
@@ -1,8 +1,58 @@
 pub struct Solution {}
 
+pub struct Tokenizer {
+    is_valid: bool,
+    stack: Vec<char>,
+}
+
+impl Tokenizer {
+    pub fn new() -> Tokenizer {
+        Tokenizer {
+            is_valid: true,
+            stack: vec![],
+        }
+    }
+    pub fn next(&mut self, c: char) {
+        match c {
+            '(' | '{' | '[' => {
+                self.stack.push(c);
+            },
+            ')' | '}' | ']' => {
+                let last = self.stack.pop();
+                match last {
+                    Some('(') => {
+                        self.is_valid = c == ')';
+                    },
+                    Some('{') => {
+                        self.is_valid = c == '}';
+                    },
+                    Some('[') => {
+                        self.is_valid = c == ']';
+                    },
+                    _ => {
+                        self.is_valid = false;
+                    }
+                }
+            }
+            _ => {}
+        }
+    }
+    pub fn is_valid(&self) -> bool {
+        self.is_valid && self.stack.len() == 0
+    }
+}
+
 impl Solution {
     pub fn is_valid(s: String) -> bool {
-        true
+        let mut tokenizer = Tokenizer::new();
+        let chars = s.chars();
+        for c in chars {
+            tokenizer.next(c);
+            if tokenizer.is_valid == false {
+                return false;
+            }
+        }
+        tokenizer.is_valid()
     }
 }
 
@@ -28,6 +78,22 @@ mod tests {
     #[test]
     fn example5() {
         assert_eq!(Solution::is_valid(String::from("{[]}")), true);
+    }
+    #[test]
+    fn example6() {
+        assert_eq!(Solution::is_valid(String::from("[")), false);
+    }
+    #[test]
+    fn example7() {
+        assert_eq!(Solution::is_valid(String::from("")), true);
+    }
+    #[test]
+    fn example8() {
+        assert_eq!(Solution::is_valid(String::from("([]")), false);
+    }
+    #[test]
+    fn example9() {
+        assert_eq!(Solution::is_valid(String::from("{{)}")), false);
     }
 }
 
