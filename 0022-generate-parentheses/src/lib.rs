02fd5f14ed867086de1aaed368ec7e5636188aa4
Toru Kobayashi <koba0004@gmail.com>
2020-06-07 00:24:12 +0900

    feat(0022): cheat

--- 0022-generate-parentheses/src/lib.rs
+++ 0022-generate-parentheses/src/lib.rs
@@ -2,7 +2,54 @@ pub struct Solution {}
 
 impl Solution {
     pub fn generate_parenthesis(n: i32) -> Vec<String> {
-        vec![]
+        let mut answer = vec![];
+        Solution::recursive(0, &mut Vec::with_capacity((n * 2) as usize), &mut answer);
+        answer
+    }
+    fn recursive(position: usize, tmp: &mut Vec<char>, answer: &mut Vec<String>) {
+        println!("{}:{:?}:{}", position, tmp, tmp.capacity());
+        if tmp.capacity() == position as usize {
+            if Solution::check(tmp) {
+                let mut s = String::new();
+                for t in tmp {
+                    s.push(*t)
+                }
+                answer.push(s);
+            }
+        } else {
+            if tmp.len() <= position {
+                tmp.push(')');
+            } else {
+                tmp[position] = ')';
+            }
+            Solution::recursive(position + 1, tmp, answer);
+            if tmp.len() <= position {
+                tmp.push('(');
+            } else {
+                tmp[position] = '(';
+            }
+            Solution::recursive(position + 1, tmp, answer);
+        }
+    }
+    fn check(target: &Vec<char>) -> bool {
+        let mut count = 0;
+        for t in target {
+            match *t {
+                '(' => {
+                    count += 1;
+                },
+                ')' => {
+                    if count == 0 {
+                        return false;
+                    }
+                    count -= 1;
+                },
+                other => {
+                    panic!("unsupported char {}", other)
+                }
+            }
+        }
+        count == 0
     }
 }
 
@@ -11,9 +58,8 @@ mod tests {
     use super::Solution;
     #[test]
     fn example1() {
-        assert_eq!(
-            Solution::generate_parenthesis(3),
-            ["((()))", "(()())", "(())()", "()(())", "()()()"]
-        )
+        let actual = Solution::generate_parenthesis(3).sort();
+        let expected = ["((()))", "(()())", "(())()", "()(())", "()()()"].sort();
+        assert_eq!(actual, expected);
     }
 }
