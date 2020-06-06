2f87cb5a8d8b388b4e40a902a20d12bf40708686
Toru Kobayashi <koba0004@gmail.com>
2020-06-07 00:39:43 +0900

    refactor(0022): remove unnecessay recursions

--- 0022-generate-parentheses/src/lib.rs
+++ 0022-generate-parentheses/src/lib.rs
@@ -3,12 +3,13 @@ pub struct Solution {}
 impl Solution {
     pub fn generate_parenthesis(n: i32) -> Vec<String> {
         let mut answer = vec![];
-        Solution::recursive(0, &mut Vec::with_capacity((n * 2) as usize), &mut answer);
+        Solution::recursive(0, &mut Vec::with_capacity((n * 2) as usize), &mut answer, 0, 0);
         answer
     }
-    fn recursive(position: usize, tmp: &mut Vec<char>, answer: &mut Vec<String>) {
-        println!("{}:{:?}:{}", position, tmp, tmp.capacity());
-        if tmp.capacity() == position as usize {
+    fn recursive(position: usize, tmp: &mut Vec<char>, answer: &mut Vec<String>, open: usize, close: usize) {
+        // println!("{}:{:?}:{}", position, tmp, tmp.capacity());
+        let capacity = tmp.capacity();
+        if capacity == position {
             if Solution::check(tmp) {
                 let mut s = String::new();
                 for t in tmp {
@@ -17,18 +18,21 @@ impl Solution {
                 answer.push(s);
             }
         } else {
+            if open > tmp.capacity() * 2 || close > tmp.capacity() * 2 {
+                return;
+            }
             if tmp.len() <= position {
                 tmp.push(')');
             } else {
                 tmp[position] = ')';
             }
-            Solution::recursive(position + 1, tmp, answer);
+            Solution::recursive(position + 1, tmp, answer, open + 1, close);
             if tmp.len() <= position {
                 tmp.push('(');
             } else {
                 tmp[position] = '(';
             }
-            Solution::recursive(position + 1, tmp, answer);
+            Solution::recursive(position + 1, tmp, answer, open, close + 1);
         }
     }
     fn check(target: &Vec<char>) -> bool {
