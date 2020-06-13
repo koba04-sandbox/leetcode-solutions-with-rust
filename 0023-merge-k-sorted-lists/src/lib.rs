6cfbecd730b53275864c7edaed16f6f113c1e60c
Toru Kobayashi <koba0004@gmail.com>
2020-06-13 22:35:12 +0900

    feat(0023): all tests have been passed!

--- 0023-merge-k-sorted-lists/src/lib.rs
+++ 0023-merge-k-sorted-lists/src/lib.rs
@@ -16,29 +16,60 @@ pub struct Solution {}
 
 impl Solution {
     pub fn merge_k_lists(lists: Vec<Option<Box<ListNode>>>) -> Option<Box<ListNode>> {
-        None
+        let mut results = vec![];
+        let mut current: Vec<&Option<Box<ListNode>>> = lists.iter().map(|n| n).collect::<Vec<_>>();
+
+        while current.iter().any(|n| n.is_some()) {
+            let i = Solution::get_latest_nodes(&current);
+            if let Some(target) = &current[i].as_ref() {
+                results.push(target.val);
+                current[i] = &target.next;
+            }
+            // println!("{:?}", current);
+        }
+        Solution::create_list_node(results)
+    }
+    pub fn create_list_node(vals: Vec<i32>) -> Option<Box<ListNode>> {
+        let mut last: Option<Box<ListNode>> = None;
+        for i in vals.into_iter().rev() {
+            let mut node = ListNode::new(i);
+            node.next = last;
+            last = Some(Box::new(node));
+        }
+        last
+    }
+    fn get_latest_nodes(lists: &Vec<&Option<Box<ListNode>>>) -> usize {
+        let mut min = None;
+        let mut min_index: usize = 0;
+        let mut i = 0;
+        for node in lists {
+            if let Some(n) = node {
+                let val = n.val;
+                match min {
+                    Some(m) => {
+                        if m > val {
+                            min = Some(val);
+                            min_index = i;
+                        }
+                    },
+                    None => {
+                        min = Some(val);
+                        min_index = i;
+                    }
+                }
+            }
+            i += 1;
+        }
+        min_index
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{ListNode, Solution};
-
-    fn create_nodes(vals: Vec<i32>) -> Option<Box<ListNode>> {
-        let mut last: Option<Box<ListNode>> = None;
-        for i in vals.into_iter().rev() {
-            let mut node = ListNode::new(i);
-            if last.is_some() {
-                // ...
-                node.next = last;
-            }
-            last = Some(Box::new(node));
-        }
-        last
-    }
+    use super::Solution;
 
     #[test]
-    fn it_works() {
+    fn example1() {
         // Input:
         // [
         // 1->4->5,
@@ -48,11 +79,35 @@ mod tests {
         // Output: 1->1->2->3->4->4->5->6
         assert_eq!(
             Solution::merge_k_lists(vec![
-                create_nodes(vec![1, 4, 5]),
-                create_nodes(vec![1, 3, 4]),
-                create_nodes(vec![2, 6])
+                Solution::create_list_node(vec![1, 4, 5]),
+                Solution::create_list_node(vec![1, 3, 4]),
+                Solution::create_list_node(vec![2, 6])
             ]),
-            create_nodes(vec![1, 1, 2, 3, 4, 4, 5, 6])
+            Solution::create_list_node(vec![1, 1, 2, 3, 4, 4, 5, 6])
         );
+    }
+    #[test]
+    fn example2() {
+        assert_eq!(
+            Solution::merge_k_lists(vec![
+                Solution::create_list_node(vec![0]),
+                Solution::create_list_node(vec![1])
+            ]),
+            Solution::create_list_node(vec![0, 1])
+        );
+    }
+    #[test]
+    fn example3() {
+        // [[],[-1,5,11],[],[6,10]]
+        assert_eq!(
+            Solution::merge_k_lists(vec![
+                Solution::create_list_node(vec![]),
+                Solution::create_list_node(vec![-1,5,11]),
+                Solution::create_list_node(vec![]),
+                Solution::create_list_node(vec![6, 10])
+            ]),
+            Solution::create_list_node(vec![-1,5,6,10,11])
+        );
+
     }
 }
