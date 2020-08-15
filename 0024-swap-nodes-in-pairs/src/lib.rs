194e1b50e96504f73e56b3784e3367f713c49e38
Toru Kobayashi <koba0004@gmail.com>
2020-08-15 22:20:27 +0900

    feat(0024): all tests have been passed!

--- 0024-swap-nodes-in-pairs/src/lib.rs
+++ 0024-swap-nodes-in-pairs/src/lib.rs
@@ -19,32 +19,54 @@ pub struct Solution {}
 
 impl Solution {
     pub fn swap_pairs(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-        None
+      let mut prev: Option<i32> = None;
+      let mut current = head;
+      let mut acc = vec![];
+      while let Some(n) = current {
+        if let Some(v) = prev {
+          acc.push(n.val);
+          acc.push(v);
+          prev = None;
+        } else {
+          prev = Some(n.val);
+        }
+        current = n.next;
+      }
+      if let Some(v) = prev {
+        acc.push(v)
+      }
+      Solution::create_list_node(acc)
     }
+    pub fn create_list_node(lists: Vec<i32>) -> Option<Box<ListNode>> {
+      let mut target: Option<Box<ListNode>> = None;
+      for val in lists.into_iter().rev() {
+          let mut node = Box::new(ListNode::new(val));
+          if target.is_some() {
+              node.next = target;
+          }
+          target = Some(node);
+      }
+      target
+  }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{Solution, ListNode};
-
-    fn create_list_node(lists: Vec<i32>) -> Option<Box<ListNode>> {
-        let mut target: Option<Box<ListNode>> = None;
-        for val in lists.into_iter().rev() {
-            let mut node = Box::new(ListNode::new(val));
-            if target.is_some() {
-                node.next = target;
-            }
-            target = Some(node);
-        }
-        target
-    }
-
+    use super::{Solution};
     #[test]
-    fn it_works() {
+    fn example1() {
         // Given 1->2->3->4, you should return the list as 2->1->4->3.
         assert_eq!(
-            Solution::swap_pairs(create_list_node(vec![1, 2, 3, 4])),
-            create_list_node(vec![2, 1, 4, 3])
+            Solution::swap_pairs(Solution::create_list_node(vec![1, 2, 3, 4])),
+            Solution::create_list_node(vec![2, 1, 4, 3])
         );
     }
-}
+    #[test]
+    fn example2() {
+      // Given 1->2->3->4, you should return the list as 2->1->4->3.
+      assert_eq!(
+          Solution::swap_pairs(Solution::create_list_node(vec![1])),
+          Solution::create_list_node(vec![1])
+      );
+  }
+  }
