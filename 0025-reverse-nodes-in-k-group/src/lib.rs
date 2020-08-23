1f19369c1d352152ae4fc62f4b56b0abd4a18fd5
Toru Kobayashi <koba0004@gmail.com>
2020-08-24 00:50:36 +0900

    feat(0025): all tests have been passed!

--- 0025-reverse-nodes-in-k-group/src/lib.rs
+++ 0025-reverse-nodes-in-k-group/src/lib.rs
@@ -20,7 +20,27 @@ pub struct Solution {}
 
 impl Solution {
     pub fn reverse_k_group(head: Option<Box<ListNode>>, k: i32) -> Option<Box<ListNode>> {
-        None
+        let mut current_index = 0;
+        let mut current_list: Vec<i32> = vec![];
+        let mut current = head;
+        let mut result: Vec<i32> = vec![];
+        while let Some(n) = current {
+            current_list.push(n.val);
+            if current_index == k - 1 {
+                for v in current_list.into_iter().rev() {
+                    result.push(v);
+                }
+                current_index = 0;
+                current_list = vec![];
+            } else {
+                current_index += 1;
+            }
+            current = n.next;
+        }
+        for v in current_list.into_iter() {
+            result.push(v);
+        }
+        Solution::create_list_node(result)
     }
     pub fn create_list_node(lists: Vec<i32>) -> Option<Box<ListNode>> {
         let mut target: Option<Box<ListNode>> = None;
@@ -53,5 +73,11 @@ mod tests {
             Solution::create_list_node(vec![3,2,1,4,5])
         )
     }
-
+    #[test]
+    fn example3() {
+        assert_eq!(
+            Solution::reverse_k_group(Solution::create_list_node(vec![1,2]), 2),
+            Solution::create_list_node(vec![2,1])
+        )
+    }
 }
