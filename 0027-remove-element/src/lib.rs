3aa339e6b4f1021582e21e3fc33746ee493dad44
Toru Kobayashi <koba0004@gmail.com>
2020-08-28 22:50:33 +0900

    feat(0027): all tests have been passed!

--- 0027-remove-element/src/lib.rs
+++ 0027-remove-element/src/lib.rs
@@ -2,6 +2,17 @@ pub struct Solution {}
 
 impl Solution {
     pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
+        if nums.len() == 0 {
+            return nums.len() as i32;
+        }
+        let mut i = 0;
+        while i < nums.len() {
+            if nums[i] == val {
+                nums.remove(i);
+            } else {
+                i += 1;
+            }
+        }
         nums.len() as i32
     }
 }
