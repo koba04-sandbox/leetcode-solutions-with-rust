50578278e57526b8d62357af20013ea0674087e1
Toru Kobayashi <koba0004@gmail.com>
2020-11-27 00:09:27 +0900

    wip

--- 0030-substring-with-concatenation-of-all-words/src/lib.rs
+++ 0030-substring-with-concatenation-of-all-words/src/lib.rs
@@ -4,11 +4,43 @@ impl Solution {
     pub fn find_substring(s: String, words: Vec<String>) -> Vec<i32> {
         vec![]
     }
+    pub fn make_patterns(words: Vec<String>) -> Vec<String> {
+        let mut result: Vec<String> = Vec::new();
+        for i in 0..words.len() {
+            Solution::recursive(i, &words, &mut result);
+        }
+        result
+    }
+    pub fn recursive(target: usize, words: &Vec<String>, result: &mut Vec<String>) {
+        let mut s = String::from(&words[target]);
+        for i in 0..words.len() {
+            if i == target {
+                continue;
+            }
+            s += &words[i];
+        }
+        result.push(s);
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::Solution;
+
+    #[test]
+    fn example0() {
+        assert_eq!(
+            Solution::make_patterns(vec![String::from("foo"), String::from("bar"), String::from("baz")]),
+            vec![
+                String::from("foobarbaz"),
+                String::from("foobazbar"),
+                String::from("barfoobaz"),
+                String::from("barbazfoo"),
+                String::from("bazfoobar"),
+                String::from("bazbarfoo")
+            ]
+        );
+    }
 
     #[test]
     fn example1() {
