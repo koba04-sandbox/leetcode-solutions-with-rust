be8a02cf7f0a5db985170d6bbf3ea64edd4ceb69
Toru Kobayashi <koba0004@gmail.com>
2021-09-30 23:30:40 +0900

    fix(0031): this answer is based on the solution 😭

--- 0031-next-permutation/src/lib.rs
+++ 0031-next-permutation/src/lib.rs
@@ -4,6 +4,52 @@ pub struct Solution {
 
 impl Solution {
     pub fn next_permutation(nums: &mut Vec<i32>) {
+        let mut sort_index = 0;
+        if let Some(i) = Solution::find_decreasing_element(&nums) {
+            // println!("i => {}", i);
+            if let Some(j) = Solution::find_larger_element(nums, i) {
+                // println!("j => {}", j);
+                Solution::swap(nums, i, j);
+                // println!("swap {:?}", nums);
+                sort_index = i + 1;
+            }
+        }
+        Solution::sort(nums, sort_index);
+    }
+    fn find_decreasing_element(nums: &Vec<i32>) -> Option<usize> {
+        let mut i = nums.len() - 1;
+        while i > 0 {
+            if nums[i] > nums[i - 1] {
+                return Some(i - 1);
+            }
+            i -= 1;
+        }
+        None
+    }
+    fn find_larger_element(nums: &Vec<i32>, target: usize) -> Option<usize> {
+        let mut i = nums.len() - 1;
+        while i > target {
+            if nums[i] > nums[target] {
+                return Some(i);
+            }
+            i -= 1;
+        }
+        None
+    }
+    fn swap(nums: &mut Vec<i32>, a: usize, b: usize) {
+        let tmp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = tmp;
+    }
+    // bubble sort
+    pub fn sort(nums: &mut Vec<i32>, start: usize) {
+        for i in start..nums.len() {
+            for j in (i + 1)..nums.len() {
+                if nums[i] > nums[j] {
+                    Solution::swap(nums, i, j);
+                }
+            }
+        }
     }
 }
 
@@ -38,5 +84,26 @@ mod tests {
         let mut target: Vec<i32> = vec![1];
         Solution::next_permutation(&mut target);
         assert_eq!(target, vec![1]);
+    }
+
+    #[test]
+    fn example5() {
+        let mut target: Vec<i32> = vec![1, 2];
+        Solution::next_permutation(&mut target);
+        assert_eq!(target, vec![2, 1]);
+    }
+
+    #[test]
+    fn example6() {
+        let mut target: Vec<i32> = vec![1, 3, 2];
+        Solution::next_permutation(&mut target);
+        assert_eq!(target, vec![2, 1, 3]);
+    }
+
+    #[test]
+    fn example7() {
+        let mut target: Vec<i32> = vec![4,2,0,2,3,2,0];
+        Solution::next_permutation(&mut target);
+        assert_eq!(target, vec![4,2,0,3,0,2,2]);
     }
 }
