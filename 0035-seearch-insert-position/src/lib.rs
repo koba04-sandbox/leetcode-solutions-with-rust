9bf013bc773a09495a89e2d97185527a6e5b4fbb
Toru Kobayashi <koba0004@gmail.com>
2022-11-20 22:12:17 +0900

    fix(0035): 35. Search Insert Position

--- 0035-seearch-insert-position/src/lib.rs
+++ 0035-seearch-insert-position/src/lib.rs
@@ -1,8 +1,25 @@
 struct Solution {}
 
 impl Solution {
+    fn search(nums: &Vec<i32>, target: i32, start: i32, end: i32) -> i32 {
+        if start > end {
+            if start as usize >= nums.len() || nums[start as usize] > target {
+                return start;
+            } else {
+                return start + 1;
+            }
+        }
+        let middle = (start + end) / 2;
+        if nums[middle as usize] == target {
+            return middle;
+        } else if nums[middle as usize] < target {
+            return Solution::search(nums, target, middle + 1, end);
+        } else {
+            return Solution::search(nums, target, start, end - 1);
+        }
+    }
     pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
-        -1
+        Solution::search(&nums, target, 0, (nums.len() - 1) as i32)
     }
 }
 
@@ -19,6 +36,7 @@ mod tests {
         assert_eq!(Solution::search_insert(nums, target), expected);
     }
 
+    #[test]
     fn example2() {
         let nums = vec![1,3,5,6];
         let target = 7;
